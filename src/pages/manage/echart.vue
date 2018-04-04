bb65483ded6e38ef886b16508bb98ba2a516197c
zxyzb <502672686@qq.com>
2018-04-04 17:56:29 +0800

    ueditor

--- src/pages/manage/echart.vue
+++ src/pages/manage/echart.vue
@@ -1,13 +1,11 @@
 <template>
 	<div>
-		<div id="myChart" :style="{width: '600px', height: '300px'}">
-		
-		</div>
-		<form action="getContent.php" id="form" method="post">
-		    <script id="editor" type="text/plain" name="content" style="width:1024px;height:500px;"></script>
+		<form action="getContent.php" id="form" method="post"">
+		    <script id="editor" type="text/plain" name="content" style="width:100%;height:300px;"></script>
 		</form>
-
-		<a @click="getContent">获取数据</a>
+		<br>
+		<el-button size="small" type="success" @click="getContent">保存</el-button>
+		
 	</div>
 </template>
 
@@ -15,39 +13,23 @@
 	export default {
 		data(){
 			return {
-				editor:null
+				editor:''
 			}
 		},
 		mounted(){
-			this.drawLine()
-			this.editor = UE.getEditor('editor',{
+			UE.delEditor('editor');
+			UE.getEditor('editor',{
 				toolbars: [[
-			        'fullscreen', 'source', '|',
-			          'fontsize', '|', 'kityformula', 'preview'
+			        'fullscreen','fontsize', '|', 'kityformula', 'preview'
 			    ]]
 			})
 		},
 		methods:{
-			drawLine(){
-		        let myChart = this.$echarts.init(document.getElementById('myChart'))
-		        myChart.setOption({
-		            title: { text: '在Vue中使用echarts' },
-		            tooltip: {},
-		            xAxis: {
-		                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-		            },
-		            yAxis: {},
-		            series: [{
-		                name: '销量',
-		                type: 'bar',
-		                data: [5, 20, 36, 10, 10, 20]
-		            }]
-		        });
-		    },
 		    getContent(){
-		    	var editor = document.getElementById('editor')
-		    	console.log(editor.value)
-		    }
+		    	this.editor.getKfContent(function(content){
+					console.log(content)
+		    	})
+		    },   
 		}
 	}
 </script>
